aa8545cadb605ab7fed82d5ce7a72e6b7ec1ac25
Lanaa12321 <lana.aldaeni@ucdenver.edu>
2026-04-19 21:43:54 -0600

    code

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,23 +1,23 @@
 import actionTypes from '../constants/actionTypes';
-//import runtimeEnv from '@mars/heroku-js-runtime-env'
-const env = process.env;
+
+const API_URL = process.env.REACT_APP_API_URL || "https://csci3916-hw3.onrender.com";
 
 function userLoggedIn(username) {
     return {
         type: actionTypes.USER_LOGGEDIN,
         username: username
-    }
+    };
 }
 
 function logout() {
     return {
         type: actionTypes.USER_LOGOUT
-    }
+    };
 }
 
 export function submitLogin(data) {
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/signin`, {
+        return fetch(`${API_URL}/signin`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -25,23 +25,26 @@ export function submitLogin(data) {
             },
             body: JSON.stringify(data),
             mode: 'cors'
-        }).then((response) => {
+        })
+        .then((response) => {
             if (!response.ok) {
                 throw Error(response.statusText);
             }
-            return response.json()
-        }).then((res) => {
+            return response.json();
+        })
+        .then((res) => {
             localStorage.setItem('username', data.username);
             localStorage.setItem('token', res.token);
 
             dispatch(userLoggedIn(data.username));
-        }).catch((e) => console.log(e));
-    }
+        })
+        .catch((e) => console.log(e));
+    };
 }
 
 export function submitRegister(data) {
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/signup`, {
+        return fetch(`${API_URL}/signup`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -49,21 +52,24 @@ export function submitRegister(data) {
             },
             body: JSON.stringify(data),
             mode: 'cors'
-        }).then((response) => {
+        })
+        .then((response) => {
             if (!response.ok) {
                 throw Error(response.statusText);
             }
-            return response.json()
-        }).then((res) => {
+            return response.json();
+        })
+        .then(() => {
             dispatch(submitLogin(data));
-        }).catch((e) => console.log(e));
-    }
+        })
+        .catch((e) => console.log(e));
+    };
 }
 
 export function logoutUser() {
     return dispatch => {
         localStorage.removeItem('username');
         localStorage.removeItem('token');
-        dispatch(logout())
-    }
+        dispatch(logout());
+    };
 }
